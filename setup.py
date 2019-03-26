4c56e88138a9677085af2d8747592e6f88169912
Bill Reus <reuster986@gmail.com>
2019-03-25 21:58:39 -0400

    Added shebang

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'hdflow',         # How you named your package folder (MyLib)
   packages = ['hdflow'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Convert CSV netflow to HDF5 in parallel.',   # Give a short description about your library
   author = 'William Reus',                   # Type in your name
